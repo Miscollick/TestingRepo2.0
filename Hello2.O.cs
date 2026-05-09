911f13ba21679a30788ba03e857548f134d73909
Miscollick <stu2201261031@uni-plovdiv.bg>
2026-05-09 14:05:06 +0300

    Testing again for commit and etc

---
+++ Hello2.O.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Hello {
+    public static void Main() {
+        Console.WriteLine("Hello from C# MOTHERFUCKER!");
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) sum += i;
+        Console.WriteLine($"Sum 1..10 = {sum}");
+    }
+}
+
+// Извикваме метода ръчно, за да го стартира скриптът:
+Hello.Main();
