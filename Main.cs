a26cf9900a0c1439cc3b234d33abfcfe6a205b44
Yumit Guler <162511320+Miscos03@users.noreply.github.com>
2026-05-30 15:48:41 +0300

    Update vowel count test string in Main.cs

--- Main.cs
+++ Main.cs
@@ -1,4 +1,4 @@
 #load "Helper.cs"
 
 Console.WriteLine(Greeter.Greet("Wonolith"));
-Console.WriteLine($"Vowels in 'multi-file test': {Greeter.CountVowels("multi-file test")}");
+Console.WriteLine($"Vowels in 'multi-file test': {Greeter.CountVowels("multi-file testing")}");
