1f51fc87b0d041621f1edf4962a6009916ab4968
Miscollick <stu2201261031@uni-plovdiv.bg>
2026-05-31 15:03:46 +0300

    Making a small change

--- Hello2.O.cs
+++ Hello2.O.cs
@@ -2,7 +2,7 @@ using System;
 
 class Hello {
     public static void Main() {
-        Console.WriteLine("Hello from C# MOTHERFUCKER!");
+        Console.WriteLine("Hello from C# Cracker!");
         int sum = 0;
         for (int i = 1; i <= 10; i++) sum += i;
         Console.WriteLine($"Sum 1..10 = {sum}");
