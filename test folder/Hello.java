3c35f36a1bf85c6c6f61677ff66aa562d172c96e
Miscollick <stu2201261031@uni-plovdiv.bg>
2026-06-02 01:43:09 +0300

    test

--- test folder/Hello.java
+++ test folder/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Wonolith says hi");
+        System.out.println("Wonolith says hi BITCHES");
     }
 }
