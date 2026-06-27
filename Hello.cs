32a6bc212bfdb980958e76b555ac0b9c2d8bdea4
Yumit G <stu2201261031@uni-plovdiv.bg>
2026-06-27 17:27:33 +0300

    changes to test

--- Hello.cs
+++ Hello.cs
@@ -1,12 +1,8 @@
 using System;
 
-class Hello {
-    public static void Main() {
-        Console.WriteLine("Hello from C#!");
-        int sum = 0;
-        for (int i = 1; i <= 11; i++) sum += i;
-        Console.WriteLine($"Sum 1..11 equals to = {sum}");
-    }
-}
+Console.WriteLine("Hello from C#!");
+int sum = 0;
+for (int i = 1; i <= 11; i++) sum += i;
+Console.WriteLine($"Sum 1..11 equals to = {sum}");
 
-Hello.Main();
+Changes again
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TestingRepo2.0
 PR sync test
+
+A simple test project to demonstrate basic functionality.
